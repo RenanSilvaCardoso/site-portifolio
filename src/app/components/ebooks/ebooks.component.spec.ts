eed2f234d1ca869ed3f666928029c49871e44a21
renaxcardoso <81385406+RenanSilvaCardoso@users.noreply.github.com>
2023-10-31 21:45:50 -0300

    add ebooks component

---
+++ src/app/components/ebooks/ebooks.component.spec.ts
@@ -0,0 +1,21 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EbooksComponent } from './ebooks.component';
+
+describe('EbooksComponent', () => {
+  let component: EbooksComponent;
+  let fixture: ComponentFixture<EbooksComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [EbooksComponent]
+    });
+    fixture = TestBed.createComponent(EbooksComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
